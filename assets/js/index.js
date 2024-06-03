54fcd73fb71be41015eae9314a8ad6ad79d92d70
1rascu <mantencion_rc@hotmail.com>
2024-06-03 05:00:10 -0400

    se modifica indes html y js se agregan funciones de sumar y restar y se coloca restrinccion para que no sean cantidades y valores negativos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,34 @@
 const precio = 400000;
-let unidades =1;
-let precioFinal;
+let cant =1;
+let tot = precio * cant;
 
 const precioUnidad = document.querySelector("#precioUnidad");
 const cantidad = document.querySelector("#cantidad");
-const sumar = document.querySelector("#sumar");
-const restar = document.querySelector("#restar");
+const btnSumar = document.querySelector("#sumar");
+const btnRestar = document.querySelector("#restar");
 const total = document.querySelector("#total");
 
-cantidad.innerHTML = unidades;
+
+cantidad.innerHTML = cant;
 precioUnidad.innerHTML = precio;
-total.innerHTML = precioFinal;
+total.innerHTML = tot;
+
+btnSumar.addEventListener("click", () => {
+     cant = cant + 1;
+     tot = precio * cant;
+     cantidad.innerHTML = cant;
+     total.innerHTML = tot; 
+});
 
 
+btnRestar.addEventListener("click", () => {
+    if (cant > 1){
+    cant = cant - 1;
+    tot = precio * cant;
+    cantidad.innerHTML = cant;
+    total.innerHTML = tot;}
+    else{alert("No puedes seleccionar menos de 1 producto")} 
+});
 
 
 
