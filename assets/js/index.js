234dca7453b263952f75858a23a713443ec5edb2
1rascu <mantencion_rc@hotmail.com>
2024-06-03 01:19:12 -0400

    se modifica el index

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+let precio = 400000
+let cantidad =0
+//let precioFinal = 
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+
+precioSpan = document.querySelector("#sumar");
+
+precioSpan = document.querySelector("#restar");
+
+spanValorTotal = document.querySelector("#valor-total")
